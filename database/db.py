3c5541e48b6926f624a1fd41ff09c3ef551485b5
Dharmanshu Jaiswal <dharmanshu.jaiswal@peoplestrong.com>
2026-03-18 19:29:30 +0530

    secure password

--- database/db.py
+++ database/db.py
@@ -5,6 +5,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="mynameis16",
+        password=os.getenv("DB_PASS"),
         database="company"
     )
