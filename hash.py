6671ae19929213e59fb379c0bb8f14616523e54d
Dharmanshu Jaiswal <dharmanshu.jaiswal@peoplestrong.com>
2026-03-18 16:16:20 +0530

    passwor to hash_password

--- hash.py
+++ hash.py
@@ -1,3 +1,20 @@
-import jwt
-print(jwt.__file__)
-print(dir(jwt))
+from services.auth_services import hash_password
+from database.db import get_connection
+
+conn = get_connection()
+cursor = conn.cursor()
+
+cursor.execute("SELECT empno, hashed_password FROM employees")
+rows = cursor.fetchall()
+
+for empno, plain_pw in rows:
+    if not plain_pw.startswith("$argon2"):
+        hashed = hash_password(plain_pw)
+
+        cursor.execute(
+            "UPDATE employees SET hashed_password = %s WHERE empno = %s",
+            (hashed, empno)
+        )
+
+conn.commit()
+conn.close()
