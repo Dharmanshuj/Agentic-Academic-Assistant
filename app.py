843b1ec9d3d8b4b5b911e721ab1b30bcb7abf0b0
LavishMahajan <lavishmahajan06@gmail.com>
2026-03-23 14:51:01 +0530

    Merge branch 'main' of https://github.com/Dharmanshuj/payroll_chatbot into lavish

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = FastAPI()
 
 # 1. Define allowed origins (your React dev server)
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174"
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:5174",
+    "http://localhost:3000"
 ]
 
 # 2. Add the Middleware
